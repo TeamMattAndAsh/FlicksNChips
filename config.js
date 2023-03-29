5dfc0ffbc5f5130493ae5ea2715545dd2f651805
Ashley <apatterson0208@gmail.com>
2023-03-29 15:37:29 -0400

    Spoonacular API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const keys = {
-    recipeKey : `7480642d2db74ba1879dc7a29e390c37`,
-    unsplashKey : `sucbq_7uPbQdHLm_6hTKBBF94HH6ZNmT--Ny8FlCbfk`,
-    movieKey: `69a3bbc0747647c0016e688032077f39`
-}
+	recipeKey: `16858861c1ce4bd4b660335ea0d2727d`,
+	unsplashKey: `sucbq_7uPbQdHLm_6hTKBBF94HH6ZNmT--Ny8FlCbfk`,
+	movieKey: `69a3bbc0747647c0016e688032077f39`,
+};
