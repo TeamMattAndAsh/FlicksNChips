8220a7a696979c4bc4654aec337d01785f24d03b
Ashley <apatterson0208@gmail.com>
2023-04-02 18:18:44 -0400

    Added images & working on card ui

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let parent = document.querySelectorAll(".animate-text");
 for (let i = 0; i < parent.length; i++) {
 	parent[i].style.width = parent[i].children[0].clientWidth + "px";
 }
+
+var swiper = new Swiper(".blog-slider", {
+	spaceBetween: 30,
+	effect: "fade",
+	loop: true,
+	mousewheel: {
+		invert: false,
+	},
+	// autoHeight: true,
+	pagination: {
+		el: ".blog-slider__pagination",
+		clickable: true,
+	},
+});
